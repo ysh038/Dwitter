f497318ad2e8edbf5544d275be2db466d6fafa56
unwind <ysh038@naver.com>
2022-05-11 18:29:20 +0900

    Dwitter

--- server/router/tweets.js
+++ server/router/tweets.js
@@ -1,6 +1,23 @@
 import express from "express";
 import "express-async-errors";
 
+const tweets = [
+    {
+        id: "1",
+        text: "화이팅",
+        createdAt: Date.now().toString(),
+        name: "Bob",
+        username: "bob",
+        url: "https://picsum.photos/200",
+    },
+];
 const router = express.Router();
 
+// GET /tweets
+// GET /tweets?username=:username
+router.get("/");
+// GET /tweets/:id
+// POST /tweets
+// PUT / tweets/:id
+// DELETE / tweets/:id
 export default router;
