efbe2d5228151f8b2eda12317ed7f5b816355e78
unwind <ysh038@naver.com>
2022-05-13 18:43:50 +0900

    nodeJS

--- server/router/tweets.js
+++ server/router/tweets.js
@@ -1,7 +1,7 @@
 import express from "express";
 import "express-async-errors";
 
-const tweets = [
+let tweets = [
     {
         id: "1",
         text: "화이팅",
@@ -10,14 +10,66 @@ const tweets = [
         username: "bob",
         url: "https://picsum.photos/200",
     },
+    {
+        id: "2",
+        text: "ㅎㅇ",
+        createdAt: Date.now().toString(),
+        name: "ysh038",
+        username: "ysh038",
+        url: "https://picsum.photos/200",
+    },
 ];
 const router = express.Router();
 
 // GET /tweets
 // GET /tweets?username=:username
-router.get("/");
+router.get("/", (req, res, next) => {
+    const username = req.query.username;
+    const data = username
+        ? tweets.filter((tweet) => tweet.username === username)
+        : tweets;
+    res.status(200).json(data);
+});
 // GET /tweets/:id
+router.get("/:id", (req, res, next) => {
+    const id = req.params.id;
+    const tweet = tweets.find((tweet) => tweet.id === id);
+    if (tweet) {
+        res.status(200).json(tweet);
+    } else {
+        res.status(404).json({ message: `Tweet ${id} not found` });
+    }
+});
 // POST /tweets
+router.post("/", (req, res, next) => {
+    const { text, name, username } = req.body;
+    const tweet = {
+        id: Date.now().toString(),
+        text,
+        createdAt: new Date(),
+        name,
+        username,
+    };
+    tweets = [tweet, ...tweets];
+    res.status(201).json(tweet);
+});
 // PUT / tweets/:id
+router.put("/:id", (req, res, next) => {
+    const id = req.params.id;
+    const text = req.body.text;
+    const tweet = tweets.find((t) => t.id === id);
+    if (tweet) {
+        tweet.text = text;
+        res.status(200).json(tweet);
+    } else {
+        res.status(404).json({ message: `Tweet ${id} not found` });
+    }
+});
 // DELETE / tweets/:id
+router.delete("/:id", (req, res, next) => {
+    const id = req.params.id;
+    tweets = tweets.filter((tweet) => tweet.id !== id);
+    res.sendStatus(204);
+});
+
 export default router;
