fda888cbe726938b69b9a5b4b39dd0a7ba2253de
unwind <ysh038@naver.com>
2022-05-14 17:02:34 +0900

    Dwitter : MVC model

---
+++ server/data/tweet.js
@@ -0,0 +1,54 @@
+let tweets = [
+    {
+        id: "1",
+        text: "화이팅",
+        createdAt: Date.now().toString(),
+        name: "Bob",
+        username: "bob",
+        url: "https://picsum.photos/200",
+    },
+    {
+        id: "2",
+        text: "ㅎㅇ",
+        createdAt: Date.now().toString(),
+        name: "Ellie",
+        username: "ellie",
+        url: "https://picsum.photos/200",
+    },
+];
+
+export function getAll() {
+    return tweets;
+}
+
+export function getAllByUsername(username) {
+    return tweets.filter((tweet) => tweet.username === username);
+}
+
+export function getById(id) {
+    return tweets.find((tweet) => tweet.id === id);
+}
+
+export function create(text, name, username) {
+    const tweet = {
+        id: Date.now().toString(),
+        text,
+        createdAt: new Date(),
+        name,
+        username,
+    };
+    tweets = [tweet, ...tweets];
+    return tweet;
+}
+
+export function update(id, text) {
+    const tweet = tweets.find((t) => t.id === id);
+    if (tweet) {
+        tweet.text = text;
+    }
+    return tweet;
+}
+
+export function remove(id) {
+    tweets = tweets.filter((tweet) => tweet.id !== id);
+}
