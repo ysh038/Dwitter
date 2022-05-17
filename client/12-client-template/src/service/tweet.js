bf079463986ad80cbe7c084d7ee3d230f2e671d5
unwind <ysh038@naver.com>
2022-05-17 17:33:56 +0900

    Express validator

--- client/12-client-template/src/service/tweet.js
+++ client/12-client-template/src/service/tweet.js
@@ -1,64 +1,32 @@
 export default class TweetService {
-    tweets = [
-        {
-            id: 1,
-            text: "드림코딩에서 강의 들으면 너무 좋으다",
-            createdAt: "2021-05-09T04:20:57.000Z",
-            name: "Bob",
-            username: "bob",
-            url: "https://picsum.photos/200",
-        },
-    ];
-
-    constructor(baseURL) {
-        this.baseURL = baseURL;
+    constructor(http) {
+        this.http = http;
     }
     async getTweets(username) {
         const query = username ? `?username=${username}` : "";
-        const response = await fetch(`${this.baseURL}/tweets${query}`, {
+        return this.http.fetch(`/tweets${query}`, {
             method: "GET",
-            headers: { "Content-Type": "application/json" },
         });
-        const data = await response.json();
-        if (response.status !== 200) {
-            throw new Error(data.message);
-        }
-        return data;
     }
 
     async postTweet(text) {
-        const response = await fetch(`${this.baseURL}/tweets`, {
+        return this.http.fetch(`/tweets`, {
             method: "POST",
-            headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ text, username: "ellie", name: "Ellie" }),
         });
-        const data = await response.json();
-        if (response.status !== 201) {
-            throw new Error(data.message);
-        }
-        return data;
     }
 
     async deleteTweet(tweetId) {
-        const response = await fetch(`${this.baseURL}/tweets/${tweetId}`, {
+        return this.http.fetch(`/tweets/${tweetId}`, {
             method: "DELETE",
             headers: { "Content-Type": "application/json" },
         });
-        if (response.status !== 204) {
-            throw new Error();
-        }
     }
 
     async updateTweet(tweetId, text) {
-        const response = await fetch(`${this.baseURL}/tweets/${tweetId}`, {
+        return this.http.fetch(`/tweets/${tweetId}`, {
             method: "PUT",
-            headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ text }),
         });
-        const data = await response.json();
-        if (response.status !== 200) {
-            throw new Error(data.message);
-        }
-        return data;
     }
 }
--- server/router/tweets.js
+++ server/router/tweets.js
@@ -1,18 +1,32 @@
 import express from "express";
 import "express-async-errors";
 import * as tweetController from "../controller/tweet.js";
+import { body } from "express-validator";
+import { validate } from "../middleware/validator.js";
+
+//validation
+//sanitization
+//Contract Testing: Client-Server
+//Proto-
 
 const router = express.Router();
 
+const validateTweet = [
+    body("text")
+        .trim()
+        .isLength({ min: 3 })
+        .withMessage("text should be at least 3 characters"),
+    validate,
+];
 // GET /tweets
 // GET /tweets?username=:username
 router.get("/", tweetController.getTweets);
 // GET /tweets/:id
 router.get("/:id", tweetController.getTweet);
 // POST /tweets
-router.post("/", tweetController.createTweet);
+router.post("/", validateTweet, tweetController.createTweet);
 // PUT / tweets/:id
-router.put("/:id", tweetController.updateTweet);
+router.put("/:id", validateTweet, tweetController.updateTweet);
 // DELETE / tweets/:id
 router.delete("/:id", tweetController.deleteTweet);
 
