91d2e0ff89c8c52d0278de6ed965db8d817dbb8c
unwind <ysh038@naver.com>
2022-06-02 17:53:59 +0900

    Dwitter : Heroku

--- server/app.js
+++ server/app.js
@@ -11,9 +11,13 @@ import { sequelize } from "./db/database.js";
 
 const app = express();
 
+const corsOption = {
+    origin: config.cors.allowedOrigin,
+    optionsSuccessStatus: 200,
+};
 app.use(express.json());
 app.use(helmet());
-app.use(cors());
+app.use(cors(corsOption));
 app.use(morgan("tiny"));
 
 app.use("/tweets", tweetsRouter);
@@ -29,6 +33,7 @@ app.use((error, req, res, next) => {
 });
 
 sequelize.sync().then(() => {
-    const server = app.listen(config.host.port);
+    console.log(`Server is started... ${new Date()}`);
+    const server = app.listen(config.port);
     initSocket(server);
 });
--- server/config.js
+++ server/config.js
@@ -17,13 +17,17 @@ export const config = {
     bcrypt: {
         saltRounds: parseInt(required("BCRYPT_SALT_ROUNDS", 12)),
     },
-    host: {
-        port: parseInt(required("HOST_PORT", 8080)),
-    },
+
+    port: parseInt(required("HOST_PORT", 8080)),
+
     db: {
         host: required("DB_HOST"),
         user: required("DB_USER"),
         database: required("DB_DATABASE"),
         password: required("DB_PASSWORD"),
+        port: required("DB_PORT"),
+    },
+    cors: {
+        allowedOrigin: required("CORS_ALLOW_ORIGIN"),
     },
 };
